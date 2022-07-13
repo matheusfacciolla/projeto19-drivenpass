1ed0c28c42626c6b27b2b95fc0ccaefaf9aa1dab
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-07-13 19:31:27 -0300

    Initializate the project with configs

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express, { json } from "express";
+import cors from "cors";
+import chalk from "chalk";
+import "express-async-errors";
+
+import "./config/setup.js";
+import router from "./routers/index.js";
+//import handleErrorsMiddleware from "./middlewares/handleErrorsMiddleware.js";
+
+const app = express();
+app.use(cors());
+app.use(json());
+app.use(router);
+//app.use(handleErrorsMiddleware);
+
+const port = +process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(chalk.bold.green(`Server is running on port: ${port}`));
+});
+
+export default app;
