32a4767264385909ae31b6ddf8747cd2d9a8be0c
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-07-21 20:44:10 -0300

    feat: add integration tests, class practice

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { json } from "express";
 import cors from "cors";
-import chalk from "chalk";
+// import chalk from "chalk/index.js";
 import "express-async-errors";
 
 import router from "./routers/index.js";
@@ -11,10 +11,5 @@ app.use(cors());
 app.use(json());
 app.use(router);
 app.use(handleErrorsMiddleware);
-
-const port = +process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(chalk.bold.green(`Server is running on port: ${port}`));
-});
 
 export default app;
